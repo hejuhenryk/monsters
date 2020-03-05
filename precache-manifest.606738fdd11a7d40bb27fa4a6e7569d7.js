7dfcb68ad422c46baeeb5b36870e1d6256ee6fe7
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2020-03-05 14:56:51 +0100

    Updates

---
+++ precache-manifest.606738fdd11a7d40bb27fa4a6e7569d7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3ae117a7ebc1ce4ba4a9abfa0f2448",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "853a6271c5266dacc1f3",
+    "url": "/monsters/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "60bb6e8517d0eee474f3",
+    "url": "/monsters/static/js/2.102280e5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/monsters/static/js/2.102280e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "853a6271c5266dacc1f3",
+    "url": "/monsters/static/js/main.84598cb3.chunk.js"
+  },
+  {
+    "revision": "1c2f7b358827c8477fbb",
+    "url": "/monsters/static/js/runtime-main.ff1887e7.js"
+  },
+  {
+    "revision": "28b1df87a1d99b9af2bd6894acdd874a",
+    "url": "/monsters/static/media/kidaha-01.28b1df87.svg"
+  },
+  {
+    "revision": "719f3e30fbc4bee3cb42848b57414b3c",
+    "url": "/monsters/static/media/kidaha-02.719f3e30.svg"
+  },
+  {
+    "revision": "51977f3c096feafb318d8c064fa7c89a",
+    "url": "/monsters/static/media/kidaha-03.51977f3c.svg"
+  },
+  {
+    "revision": "aa7a524a27ac521b8082339464dd572a",
+    "url": "/monsters/static/media/kidaha-04.aa7a524a.svg"
+  },
+  {
+    "revision": "7f120c30f5dc8d661b9d78350f2bafe8",
+    "url": "/monsters/static/media/kidaha-05.7f120c30.svg"
+  },
+  {
+    "revision": "ed9e2817806c2601a198d32f8a45f6df",
+    "url": "/monsters/static/media/kidaha-06.ed9e2817.svg"
+  },
+  {
+    "revision": "0c5e666e25dc58c9350b2437d1e7977f",
+    "url": "/monsters/static/media/kidaha-07.0c5e666e.svg"
+  },
+  {
+    "revision": "7f01145b2e28e3d1da12eff489885777",
+    "url": "/monsters/static/media/kidaha-08.7f01145b.svg"
+  },
+  {
+    "revision": "c7d2c18ab9d1d3b249c96a4296f6288b",
+    "url": "/monsters/static/media/kidaha-09.c7d2c18a.svg"
+  },
+  {
+    "revision": "1ba093e03398adf0e83899d2a9ca8c98",
+    "url": "/monsters/static/media/kidaha-10.1ba093e0.svg"
+  },
+  {
+    "revision": "044de6ee0e25e2d7a11991e606090998",
+    "url": "/monsters/static/media/kidaha-11.044de6ee.svg"
+  },
+  {
+    "revision": "f9066f87ee31a5d5998f17c3803cbfb5",
+    "url": "/monsters/static/media/kidaha-12.f9066f87.svg"
+  }
+]);
